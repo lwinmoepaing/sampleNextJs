27ed9cd91daf7c7f9d6b8b4c5ae73ecaaf0cb6bd
Lwin Moe Paing <lwinmoepaingv@marathonmyanmar.com>
2020-02-19 17:04:23 +0630

    Update Custom Export

--- next.config.js
+++ next.config.js
@@ -7,6 +7,21 @@ module.exports = {
     BASE_API_URL: BASE_API_URL || 'default route',
     PORT,
   },
-  // assetPrefix: '/src',
-  rootServeDir: '/src',
+
+  // Exporting Static
+  exportTrailingSlash: true,
+  async exportPathMap() {
+    const paths = {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/user': { page: '/user' },
+    }
+    const data = await Array.from({ length: 10 }, (_, i) => i + 1)
+
+    data.forEach((id) => {
+      paths[`/user/${id}`] = { page: '/user/[id]', query: { id } }
+    })
+
+    return paths
+  },
 }
